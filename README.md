8f66f509595020d4731ff6f63acc41b183447c92
“Amrit <amritsairam@gmail.com>
2023-09-01 00:28:31 +0530

    data ingestion done

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cell-segmentation-using-yolo-v8
 end to end project of cell segmentation using yolo v8
+
+#workflows
+
+1.constants
+2.entity
+3.components
+4.pipeline
+5.app.py
+
---
+++ cellsegmentation/constant/training_pipeline/__init__.py
@@ -0,0 +1,7 @@
+ARTIFACTS_DIR :str ='artifacts'#main folder
+
+DATA_INGESTION_DIR_NAME:str ='data_ingestion'
+
+DATA_INGESTION_FEATURE_STORE_DIR:str ='feature_store'#this will contain the unziped file
+
+DATA_DOWNLOAD_URL:str ='https://drive.google.com/file/d/12td791izDSn8XDN2AW9ZL_4a8-uSKigl/view?usp=drive_link'
