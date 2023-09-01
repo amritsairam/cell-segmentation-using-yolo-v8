afe0b99830b65f5a8c2ecc7dd344bf7c9f389739
“Amrit <amritsairam@gmail.com>
2023-09-02 01:26:57 +0530

    data validation completed

--- cellsegmentation/constant/training_pipeline/__init__.py
+++ cellsegmentation/constant/training_pipeline/__init__.py
@@ -5,3 +5,14 @@ DATA_INGESTION_DIR_NAME:str ='data_ingestion'
 DATA_INGESTION_FEATURE_STORE_DIR:str ='feature_store'#this will contain the unziped file
 
 DATA_DOWNLOAD_URL:str ='https://drive.google.com/file/d/12td791izDSn8XDN2AW9ZL_4a8-uSKigl/view?usp=drive_link'
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
---
+++ cellsegmentation/pipeline/training_pipeline.py
@@ -0,0 +1,82 @@
+import sys, os
+from cellsegmentation.logger import logging
+from cellsegmentation.exception import AppException
+from cellsegmentation.components.data_ingestion import DataIngestion
+from cellsegmentation.components.data_validation import DataValidation
+# from cellsegmentation.components.model_trainer import ModelTrainer
+
+
+from cellsegmentation.entity.config_entity import (DataIngestionConfig,
+                                                 DataValidationConfig)
+
+from cellsegmentation.entity.artifacts_entity import (DataIngestionArtifact,
+                                                    DataValidationArtifact)
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
+
+
+    
+    def start_data_ingestion(self)-> DataIngestionArtifact:
+        try: 
+            logging.info(
+                "Entered the start_data_ingestion method of TrainPipeline class"
+            )
+            logging.info("Getting the data from URL")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config =  self.data_ingestion_config
+            )
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Got the data from URL")
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+        
+
+
+    
+    def start_data_validation(self, data_ingestion_artifact) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+
+    
+
+    def run_pipeline(self) -> None:
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+
+        
+        except Exception as e:
+            raise AppException(e, sys)
