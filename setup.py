cf429f78082bcef894b48aff6c5a7f3c3d43704b
Sairam Ramjidas <sairamramjidas@Sairams-MacBook-Pro.local>
2023-09-03 17:25:31 +0530

    best.pt created

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'cellSegmentation',
-    version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
-    packages= find_packages(),
-    install_requires = []
-
+    name='cellSegmentation',
+    version='0.0.0',
+    author='amrit sairam',
+    author_email='amritsairam@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
 )
